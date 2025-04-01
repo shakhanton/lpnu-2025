7a73331bdbe7e3ff3a0b8005f53b23255542ebf6
Anton Shakh <shakh@quintagroup.org>
2025-04-01 09:46:04 +0300

    add save course lambda

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -14,6 +14,14 @@ module "label_get_all_authors" {
   name    = "get-all-authors"
 }
 
+module "label_save_course" {
+  source = "cloudposse/label/null"
+  # Cloud Posse recommends pinning every module to a specific version
+  version = "0.25.0"
+  context = var.context
+  name    = "save-course"
+}
+
 module "lambda_get_all_authors" {
   source        = "terraform-aws-modules/lambda/aws"
   version       = "7.20.1"
@@ -34,6 +42,32 @@ module "lambda_get_all_authors" {
       effect    = "Allow",
       actions   = ["dynamodb:Scan"],
       resources = ["${var.authors_table_arn}"]
+    }
+  }
+
+  tags = module.label.tags
+}
+
+module "lambda_save_course" {
+  source        = "terraform-aws-modules/lambda/aws"
+  version       = "7.20.1"
+  function_name = module.label_save_course.id
+  description   = "My awesome lambda function"
+  handler       = "index.handler"
+  runtime       = "nodejs16.x"
+
+  source_path = "${path.module}/src/save-course"
+
+  environment_variables = {
+    TABLE_COURSES = var.courses_table
+  }
+  attach_policy_statements = true
+
+  policy_statements = {
+    dynamodb = {
+      effect    = "Allow",
+      actions   = ["dynamodb:PutItem"],
+      resources = ["${var.courses_table_arn}"]
     }
   }
 
--- modules/lambda/src/get-all-authors/index.js
+++ modules/lambda/src/get-all-authors/index.js
@@ -13,6 +13,9 @@ exports.handler = (event, context, callback) => {
       console.log(err);
       callback(err);
     } else {
+      const authors = data.Items.map(item => {
+        return { id: item.id.S, firstName: item.firstName.S, lastName: item.lastName.S };
+      });
       callback(null, data);
     }
   });
